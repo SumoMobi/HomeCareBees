44197aa980a337c14583ee31f9caeea693e7ecc8
Jean Minnaar <jean_minnaar@hotmail.com>
2019-05-26 13:18:29 -0700

    Removed key code from Github.

--- Insights/Crypto.cs
+++ Insights/Crypto.cs
@@ -5,30 +5,9 @@ using System.Text;
 
 namespace Hcb.Insights
 {
-    public class Crypto
+    public partial class Crypto
     {
         static SecureString key = new SecureString();
-        internal static string GetKey()
-        {
-            key.AppendChar('&');
-            key.AppendChar('*');
-            key.AppendChar('T');
-            key.AppendChar('h');
-            key.AppendChar('%');
-            key.AppendChar('$');
-            key.AppendChar('L');
-            key.AppendChar('1');
-            key.AppendChar('2');
-            key.AppendChar('+');
-            key.AppendChar('+');    //11
-            key.AppendChar('*');
-            key.AppendChar('-');
-            key.AppendChar('_');
-            key.AppendChar('=');
-            key.AppendChar(')');    //16
-            string plainStr = new System.Net.NetworkCredential(string.Empty, key).Password;
-            return plainStr;
-        }
         public static string Encrypt(string data)
         {
             TripleDESCryptoServiceProvider DES = new TripleDESCryptoServiceProvider();
