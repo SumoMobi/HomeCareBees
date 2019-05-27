c35f5aa66b653a7868e4e6b75780c59ee11baa5e
Jean Minnaar <jean_minnaar@hotmail.com>
2019-05-26 20:51:32 -0700

    /Index to redirect to /

--- Insights/Pages/Index.cshtml.cs
+++ Insights/Pages/Index.cshtml.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Hcb.Insights.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGet()
         {
-
+            if (Request.Path != "/")
+            {
+                return RedirectToPagePermanent("");
+            }
+            return Page();
         }
     }
 }
