211619b7350764db1e9ead9a2e37044cf1ca16fa
JeanMinnaar16GB <jean_minnaar@hotmail.com>
2019-06-16 16:00:22 -0700

    1) Added ownership proof for Bing submission 2) Added styles section to master page 3) Improved best time to call as will as validation thereof. 4) Made home page css home page specific.

--- Insights/Pages/ContactUs.cshtml.cs
+++ Insights/Pages/ContactUs.cshtml.cs
@@ -1,25 +1,78 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Mail;
+using System.Text;
 
 namespace Hcb.Insights.Pages
 {
+    public class CheckboxIdAndLabel
+    {
+        public string Id { get; set; }
+        public string Label { get; set; }
+    }
+    public class BestTimeTocall
+    {
+        public string DayLabel { get; set; }
+        public List<CheckboxIdAndLabel> CheckboxIdsAndLabels { get; set; }
+    }
     public class ContactUsModel : PageModel
     {
+        public List<BestTimeTocall> BestTimesToCall = new List<BestTimeTocall>();
+
         public void OnGet()
         {
             ViewData["thankYouDisplayStyle"] = "none";
             ViewData["contactUsActive"] = "active";
+
+            SetUpBestTimesToCall();
         }
         public void OnPost()
         {
+            StringBuilder sb = new StringBuilder();
+            foreach(string key in Request.Form.Keys)
+            {
+                sb.Append($"{key}[{Request.Form[key]}]\n");
+            }
             Email.Send(
                 Request.Form["emailAddress"],
                 "inquiries@homecarebees.com",
-                $"First Name: {Request.Form["firstName"]}\nLast Name: {Request.Form["lastName"]}\nPhone Number: {Request.Form["phoneNumber"]}\nCall Times: {string.Join(',', Request.Form["callTimes"])}\nInquiry:\n{Request.Form["inquiry"]}");
+                sb.ToString());
 
             ViewData["thankYouDisplayStyle"] = "";
+
+            SetUpBestTimesToCall();
+        }
+        private void SetUpBestTimesToCall()
+        {
+            foreach (string dayOfWeek in Enum.GetNames(typeof(DayOfWeek)))
+            {
+                List<CheckboxIdAndLabel> checkboxIdsAndLabels = new List<CheckboxIdAndLabel>()
+                {
+                    new CheckboxIdAndLabel
+                    {
+                        Id = $"morning{dayOfWeek}",
+                        Label = "Morning"
+                    },
+                    new CheckboxIdAndLabel
+                    {
+                        Id = $"afternoon{dayOfWeek}",
+                        Label = "Afternoon"
+                    },
+                    new CheckboxIdAndLabel
+                    {
+                        Id = $"evening{dayOfWeek}",
+                        Label = "Evening"
+                    }
+                };
+                BestTimesToCall.Add(new BestTimeTocall
+                {
+                    DayLabel = dayOfWeek,
+                    CheckboxIdsAndLabels = checkboxIdsAndLabels
+                });
+            }
         }
     }
 }
---
+++ Insights/wwwroot/js/contactUs.js
@@ -0,0 +1,31 @@
+﻿var _hcb = _hcb || {};
+_hcb.contactUs = {
+    setValueForDummy: function () {
+        var checkboxes = $("form .form-row :checkbox");
+        var dummyInput = $("#dummy");
+        dummyInput.removeClass("is-invalid");
+        dummyInput.val("");
+        checkboxes.each(function () {
+            if (this.checked) {
+                dummyInput.val("true");
+                return false;
+            }
+        });
+        if (dummyInput.val() === "") {
+            dummyInput.addClass("is-invalid");
+        }
+    },
+    submitClicked: function (evt) {
+        var dummyInput = $("#dummy");
+        if (dummyInput.val() === "") {
+            dummyInput.addClass("is-invalid");
+            evt.preventDefault();
+            evt.stopPropagation();
+        }
+    }
+}
+$(document).ready(function () {
+    $("form .form-row :checkbox").on("click", function () {
+        _hcb.contactUs.setValueForDummy();
+    });
+});
