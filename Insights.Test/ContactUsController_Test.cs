dcabf97724a66de07991cbaf2f8f4afed916ee8e
Jean Minnaar <jean_minnaar@hotmail.com>
2020-01-23 20:51:24 -0700

    Insignificant changes

--- Insights.Test/ContactUsController_Test.cs
+++ Insights.Test/ContactUsController_Test.cs
@@ -104,12 +104,6 @@ namespace Hcb.Insights.Test
             contextForPage.Session = controller.HttpContext.Session;
             ActionContext actionContext = new ActionContext(contextForPage, new RouteData(), new PageActionDescriptor(), modelState);
             PageContext pageContext = new PageContext(actionContext);
-            //pageContext.HttpContext.Request.Body = new MemoryStream();
-            //bytes = Encoding.ASCII.GetBytes("promotionCode=&firstName=Jean&lastName=Minnaar&phoneNumber=4802086807&patientAgeGroup=76%2B&morningSunday=on&morningMonday=on&emailAddress=jean.minnaar%40homecarebees.com&inquiry=fffffffffffff&reCaptchaToken=03AOLTBLSWS0kCcPPc6Y6sLE6tBX28cfp2pSMb1qWt23FTIUtrdGCz3ZjeNQSoQNZVzL9hamZpcqEOxv8pNdNyikDGldu6LYDr4m280fo7UGLxH1vzLSj2upfYGbCNxlfncoq8e1YSRabbq1aaVfbgDOrZlC-pVes8jEdrlyLBAH1Qp8SGv8pzB2Bk7uzDpMmLmx0sr4uEbT-MAmeHpx9ENI6ZuSHdRnsliYc2sN-d623msOmc1P8e6X6vnIJ9B48A0KRP_vjJWNrn1ybgwkhi18bGG4T0zXLOaHz02dD9GR11-4_uV4xTXKN5aCiWzdntBSF7zQ8j50e1TV66j89dAPlsi8uXyM3JAmKZdJimIy3hx6_Z7I4WLtA30zhs-IJQpOoQNhJ9n0cUfeAZ1KtRY2SVcJfbNAmN9JcnY5wldMUDrOfNJQrSpwG_hO-nd_W-XeEpLaAI4d90QmpUAYA8bFWS3QA9JsnapdTM09xwQ9HtrJ6DYVIcvGN_85dNZr2n6iyVTp3RY5bCzUwWlulSsTjWt014SUIjsQ&__RequestVerificationToken=CfDJ8Bo_msqJfLdAkBTKkmWwgv5I4s2uVJdnhNcO1X98IvwOJqYA-vMEZHCFCc_RuEYCAj4Vo2Z5VLYWla1T0QCWzTOnDYW-rY0dGHDTUZ9ds-GxLH1wbie4oa11bYwWWUU53EXgu6yzvUUcarSa3ohtsDc");
-            //bytes = Encoding.ASCII.GetBytes("reCaptchaToken=somethingelse");
-            //pageContext.HttpContext.Request.Body.Write(bytes, 0, bytes.Length);
-            //pageContext.HttpContext.Request.Body.Flush();
-            //pageContext.HttpContext.Request.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
 
             Pages.ContactUsModel contactUs = new Pages.ContactUsModel()
             {
@@ -136,17 +130,12 @@ namespace Hcb.Insights.Test
             Environment.SetEnvironmentVariable("_IsTokenValid", "true");
             bool result = controller.Post();
             Assert.AreEqual(true, result);
-            //Now make the form post call.
+            //Now make the validation call.
             ModelStateDictionary modelState = new ModelStateDictionary();
             HttpContext contextForPage = new DefaultHttpContext();
             contextForPage.Session = controller.HttpContext.Session;
             ActionContext actionContext = new ActionContext(contextForPage, new RouteData(), new PageActionDescriptor(), modelState);
             PageContext pageContext = new PageContext(actionContext);
-            //pageContext.HttpContext.Request.Body = new MemoryStream();
-            //bytes = Encoding.ASCII.GetBytes("reCaptchaToken=sometesttoken");
-            //pageContext.HttpContext.Request.Body.Write(bytes, 0, bytes.Length);
-            //pageContext.HttpContext.Request.Body.Flush();
-
             Pages.ContactUsModel contactUs = new Pages.ContactUsModel()
             {
                 PageContext = pageContext,
--- Insights/Pages/ContactUs.cshtml.cs
+++ Insights/Pages/ContactUs.cshtml.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
 using System.Collections.Generic;
